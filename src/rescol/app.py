c20b811cf58cbb356b6678d323d17e7497c41f49
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2025-06-07 13:06:50 +0100

    feat: revamp project (#12)
    * build: bump textual version
    
    * refactor: simplify handling band selects
    
    * refactor: remove unused band message
    
    * refactor: change reactives to vars
    
    * refactor: simplfy resistor css
    
    * refactor: simplify resistance input css

--- src/rescol/app.py
+++ src/rescol/app.py
@@ -4,9 +4,8 @@ from enum import Enum
 
 from textual import on
 from textual.app import App, ComposeResult
-from textual.containers import Center, Horizontal, Vertical
-from textual.message import Message
-from textual.reactive import reactive
+from textual.containers import Center, Horizontal, VerticalGroup
+from textual.reactive import var
 from textual.widget import Widget
 from textual.widgets import Input, Label, Select, Static
 
@@ -24,6 +23,29 @@ class ColorCode(Enum):
     WHITE = 9
 
 
+class BandSelect(Select):
+    def __init__(
+        self,
+        *,
+        name: str | None = None,
+        id: str | None = None,
+        classes: str | None = None,
+        disabled: bool = False,
+    ):
+        options = [
+            (f"{color_code.value} ({color_code.name.title()})", color_code)
+            for color_code in ColorCode
+        ]
+        super().__init__(
+            options,
+            allow_blank=False,
+            name=name,
+            id=id,
+            classes=classes,
+            disabled=disabled,
+        )
+
+
 class BandSelects(Widget):
     DEFAULT_CSS = """
     BandSelects {
@@ -31,60 +53,28 @@ class BandSelects(Widget):
         height: auto;
         layout: horizontal;
         margin: 1;
-    }
 
-    BandSelects Vertical {
-        width: 22;
-        height: auto;
-    }
+        & > VerticalGroup {
+            width: 20;
 
-    BandSelects Label {
-        text-style: bold;
-        text-align: center;
-        width: 100%;
-    }
-
-    BandSelects SelectOverlay {
-        max-height: 12;
+            & > Label {
+            text-style: bold;
+            text-align: center;
+            width: 100%;
+            }
+        }
     }
     """
 
     def compose(self) -> ComposeResult:
-        digit_options = [
-            (f"{color_code.value} ({color_code.name.title()})", color_code)
-            for color_code in ColorCode
-        ]
+        for digit in ["First", "Second"]:
+            with VerticalGroup():
+                yield Label(f"{digit} digit")
+                yield BandSelect()
 
-        superscripts = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"]
-        multiplier_options = [
-            (
-                f"x10{superscripts[color_code.value]} ({color_code.name.title()})",
-                color_code,
-            )
-            for color_code in ColorCode
-        ]
-
-        with Vertical():
-            yield Label("First Digit")
-            yield Select(
-                digit_options,
-                id="digit-one-select",
-                allow_blank=False,
-            )
-        with Vertical():
-            yield Label("Second Digit")
-            yield Select(
-                digit_options,
-                id="digit-two-select",
-                allow_blank=False,
-            )
-        with Vertical():
-            yield Label("Multiplier")
-            yield Select(
-                multiplier_options,
-                id="multiplier-select",
-                allow_blank=False,
-            )
+        with VerticalGroup():
+            yield Label("Number of zeros")
+            yield BandSelect(classes="multiplier")
 
 
 class Band(Static):
@@ -96,21 +86,10 @@ class Band(Static):
     }
     """
 
-    value = reactive(ColorCode.BLACK)
+    value = var(ColorCode.BLACK)
 
-    class Changed(Message):
-        def __init__(self, band: Band, value: ColorCode) -> None:
-            super().__init__()
-            self.value: ColorCode = value
-            self.band: Band = band
-
-        @property
-        def control(self) -> Band:
-            return self.band
-
-    def watch_value(self, value: ColorCode) -> None:
+    def watch_value(self) -> None:
         self.styles.background = self.value.name.lower()
-        self.post_message(self.Changed(self, value))
 
 
 class ToleranceBand(Static):
@@ -127,84 +106,42 @@ class ToleranceBand(Static):
 class Resistor(Widget):
     DEFAULT_CSS = """
     Resistor {
+        layout: horizontal;
         height: 5;
         width: 35;
+        padding: 0 2;
         background: #e1bc7b;
-        layout: horizontal;
-    }
-
-    Resistor #value-bands {
-        width: 24;
-        margin: 0 2;
     }
     """
 
-    value = reactive(0)
-    digit_one = reactive(0)
-    digit_two = reactive(0)
-    multiplier = reactive(pow(10, 0))
-
-    class Changed(Message):
-        def __init__(self, resistor: Resistor, value: int) -> None:
-            super().__init__()
-            self.value: int = value
-            self.resistor: Resistor = resistor
-
-        @property
-        def control(self) -> Resistor:
-            return self.resistor
-
     def compose(self) -> ComposeResult:
-        with Horizontal(id="value-bands"):
-            yield Band(id="digit-one-band")
-            yield Band(id="digit-two-band")
-            yield Band(id="multiplier-band")
+        with Horizontal():
+            for _ in range(3):
+                yield Band()
         yield ToleranceBand()
 
-    def compute_value(self) -> int:
-        number = (self.digit_one * 10) + self.digit_two
-        return number * self.multiplier
 
-    def watch_value(self, value: int) -> None:
-        self.post_message(self.Changed(self, value))
-
-    @on(Band.Changed)
-    def on_band_changed(self, event: Band.Changed) -> None:
-        color_code = event.value
-        assert isinstance(color_code, ColorCode)
-
-        if event.band.id == "digit-one-band":
-            self.digit_one = color_code.value
-        elif event.band.id == "digit-two-band":
-            self.digit_two = color_code.value
-        elif event.band.id == "multiplier-band":
-            self.multiplier = pow(10, int(color_code.value))
-        else:
-            raise NotImplementedError()
-
-
-class ResistorValueDisplay(Widget):
+class ResistanceInput(Widget):
     DEFAULT_CSS = """
-    ResistorValueDisplay {
+    ResistanceInput {
+        layout: horizontal;
         height: auto;
         width: auto;
         margin: 1;
-        layout: horizontal;
-    }
 
-    ResistorValueDisplay Input {
-        width: 20;
-    }
+        & > Input {
+            width: 20;
+        }
 
-    ResistorValueDisplay Label {
-        width: auto;
-        height: 3;
-        content-align: center middle;
-        text-style: bold;
+        & > Label {
+            height: 3;
+            content-align: center middle;
+            text-style: bold;
+        }
     }
     """
 
-    value = reactive(0, init=False)
+    value = var(0, init=False)
 
     def compose(self) -> ComposeResult:
         yield Input(str(self.value), disabled=True)
@@ -228,29 +165,30 @@ class ResistorColorCodeApp(App):
         with Center():
             yield Resistor()
         with Center():
-            yield ResistorValueDisplay()
+            yield ResistanceInput()
 
-    @on(Select.Changed)
-    def on_band_select_changed(self, event: Select.Changed) -> None:
-        color_code = event.value
-        assert isinstance(color_code, ColorCode)
+    @on(BandSelect.Changed)
+    def on_band_select_changed(self) -> None:
+        band_selects = self.query(BandSelect)
+        bands = self.query(Band)
 
-        if event.select.id == "digit-one-select":
-            band = self.query_one("#digit-one-band", Band)
-            band.value = color_code
-        elif event.select.id == "digit-two-select":
-            band = self.query_one("#digit-two-band", Band)
-            band.value = color_code
-        elif event.select.id == "multiplier-select":
-            band = self.query_one("#multiplier-band", Band)
-            band.value = color_code
-        else:
-            raise NotImplementedError()
+        value = 0
+        multiplier = 0
+        for band_select, band in zip(band_selects, bands):
+            color_code = band_select.value
+            assert isinstance(color_code, ColorCode)
 
-    @on(Resistor.Changed)
-    def on_resistor_changed(self, event: Resistor.Changed) -> None:
-        value_label = self.query_one(ResistorValueDisplay)
-        value_label.value = event.value
+            band.value = color_code
+
+            if band_select.has_class("multiplier"):
+                multiplier = 10**color_code.value
+            else:
+                value = value * 10 + color_code.value
+
+        value *= multiplier
+
+        resistance_input = self.query_one(ResistanceInput)
+        resistance_input.value = value
 
 
 def run() -> None:
